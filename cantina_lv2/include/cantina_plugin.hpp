388346376338f5744e4a10ad94fa944ce125023a
piptouque <63.pthiel@gmail.com>
2021-06-02 22:31:38 +0200

    Initial commit #2.

---
+++ cantina_lv2/include/cantina_plugin.hpp
@@ -0,0 +1,58 @@
+//
+// Created by binabik on 30/05/2021.
+//
+
+#ifndef CANTINA_LV2_INCLUDE_CANTINA_PLUGIN_HPP
+#define CANTINA_LV2_INCLUDE_CANTINA_PLUGIN_HPP
+
+#include "lv2/core/lv2.h"
+#include "lv2/atom/atom.h"
+#include "lv2/patch/patch.h"
+#include "lv2/log/log.h"
+#include "lv2/log/logger.h"
+#include "lv2/midi/midi.h"
+#include "lv2/urid/urid.h"
+
+#include <cant/Cantina.hpp>
+#include <cant/pan/Pantoufle.hpp>
+
+enum EPortIndex {
+    CANTINA_CONTROL = 0,
+    CANTINA_NUMBERHARMONICS = 1,
+    CANTINA_GAIN = 2,
+    CANTINA_INPUT = 3,
+    CANTINA_OUTPUT = 4
+} ;
+
+struct CantinaURIs {
+    LV2_URID midi_Event;
+};
+
+struct CantinaPlugin {
+    // Features
+    LV2_URID_Map * map;
+    LV2_Log_Logger  logger;
+
+    CantinaURIs uris;
+
+    struct {
+        LV2_Atom_Sequence  const * control;
+        float const * gain;
+        int const *n_voices;
+        float const * input;
+        float * output;
+    } ports;
+
+    //Cantina
+    std::unique_ptr<cant::Cantina> cantina;
+    float** outputBuffers;
+    uint32_t cachedBlockSize;
+
+};
+
+static inline void
+map_cantina_uris(LV2_URID_Map * map, CantinaURIs * uris) {
+    uris->midi_Event = map->map(map->handle, LV2_MIDI__MidiEvent);
+}
+
+#endif //CANTINA_LV2_INCLUDE_CANTINA_PLUGIN_HPP
---
+++ cantina_lv2/source/cantina_plugin.cpp
@@ -0,0 +1,197 @@
+/**
+ * following: https://lv2plug.in/book/
+ */
+
+#include <memory>
+#include <cmath>
+
+#include <cantina_plugin.hpp>
+
+#include "lv2/atom/util.h"
+#include "lv2/core/lv2_util.h"
+
+static constexpr float db_gain_to_coef(float gain) {
+    return gain > -90.0f ? std::pow(10.0f, gain * 0.05f) : 0.0f;
+}
+
+static LV2_Handle
+instantiate([[maybe_unused]] LV2_Descriptor const * descriptor,
+            [[maybe_unused]] double rate,
+            [[maybe_unused]] char const* bundle_path,
+            LV2_Feature const * const * features) {
+    auto self = static_cast<CantinaPlugin *>(calloc(1, sizeof(CantinaPlugin)));
+    if (!self) {
+        // ERROR
+        return nullptr;
+    }
+    // Scan host features for URID map
+    char const * missing = lv2_features_query(
+            features,
+            LV2_LOG__log, &self->logger.log, false,
+            LV2_URID__map, &self->map, true,
+            nullptr);
+    lv2_log_logger_set_map(&self->logger, self->map);
+    if (missing) {
+        lv2_log_error(&self->logger, "Missing feature <%s> \n", missing);
+        free(self);
+        return nullptr;
+    }
+
+    map_cantina_uris(self->map, &self->uris);
+
+
+    try {
+        self->cantina = std::make_unique<cant::Cantina>(
+                *self->ports.n_voices,
+                rate,
+                1 // channel
+                );
+        self->cachedBlockSize = 0; // init, whatever.
+
+        self->cantina->setCustomClock([&self, rate]() -> double{
+            // last block size
+            return rate * self->cachedBlockSize;
+        });
+    } catch (cant::CantinaException const & e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return reinterpret_cast<LV2_Handle>(self);
+}
+
+static void
+cleanup(LV2_Handle instance) {
+    free(instance);
+}
+
+void update_output_buffers(CantinaPlugin * self, uint32_t block_size) {
+    if (block_size <= self->cachedBlockSize) {
+        return;
+    }
+    for (std::size_t voice = 0; voice < self->cantina->getNumberHarmonics(); ++voice) {
+        self->outputBuffers[voice] = static_cast<float *>(
+                realloc(self->outputBuffers[voice], block_size * sizeof(float)));
+    }
+    self->cachedBlockSize = block_size;
+}
+
+void merge_output(CantinaPlugin * self, float coef, uint32_t block_size) {
+    for (uint32_t i = 0; i < block_size; ++i) {
+        self->ports.output[i] = 0.f;
+        for (std::size_t voice = 0; voice < self->cantina->getNumberHarmonics(); ++voice) {
+            self->ports.output[i] += self->outputBuffers[voice][i];
+        }
+        self->ports.output[i] *= coef;
+    }
+}
+
+static void
+connect_port(LV2_Handle instance, uint32_t port, void * data) {
+    auto self = reinterpret_cast<CantinaPlugin *>(instance);
+    switch (static_cast<EPortIndex>(port)) {
+        case CANTINA_CONTROL:
+            self->ports.control = reinterpret_cast<LV2_Atom_Sequence const *>(data);
+            break;
+        case CANTINA_NUMBERHARMONICS:
+            self->ports.n_voices = reinterpret_cast<int const *>(data);
+            // todo: reset Cantina
+            break;
+        case CANTINA_GAIN:
+            self->ports.gain = reinterpret_cast<float const*>(data);
+            break;
+        case CANTINA_INPUT:
+            self->ports.input = reinterpret_cast<float const*>(data);
+            break;
+        case CANTINA_OUTPUT:
+            self->ports.output = reinterpret_cast<float *>(data);
+            break;
+    }
+}
+
+static void
+activate(LV2_Handle instance) {
+    // todo
+    auto self = reinterpret_cast<CantinaPlugin *>(instance);
+
+}
+
+static void
+deactivate([[maybe_unused]] LV2_Handle instance) {
+
+}
+
+static void const *
+extension_data([[maybe_unused]] char const * uri) {
+    return nullptr;
+}
+
+static void
+run(LV2_Handle instance, uint32_t n_samples) {
+    auto self = reinterpret_cast<CantinaPlugin *>(instance);
+
+    update_output_buffers(self, n_samples);
+
+    // Notes and controls
+    LV2_Atom_Sequence  const * seq = self->ports.control;
+    LV2_ATOM_SEQUENCE_FOREACH(seq, ev) {
+        if (ev->body.type != self->uris.midi_Event) { continue; }
+        auto msg = reinterpret_cast<uint8_t const *>(ev + 1);
+        switch(lv2_midi_message_type(msg)) {
+            case LV2_MIDI_MSG_NOTE_ON:
+            case LV2_MIDI_MSG_NOTE_OFF:
+                try {
+                    self->cantina->receiveNote(
+                            cant::pan::MidiNoteInputData(
+                                    msg[0],
+                                    static_cast<int8_t>(msg[1]),
+                                    static_cast<int8_t>(msg[2])));
+                }
+                catch (cant::CantinaException const & e) {
+                    std::cerr << e.what() << std::endl;
+                }
+                break;
+            case LV2_MIDI_MSG_CONTROLLER:
+                try {
+                    self->cantina->receiveControl(
+                            cant::pan::MidiControlInputData(
+                                    msg[0],
+                                    static_cast<uint8_t>(msg[1]),
+                                    static_cast<uint8_t>(msg[2])));
+                }
+                catch (cant::CantinaException const & e) {
+                    std::cerr << e.what() << std::endl;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    try {
+        self->cantina->update();
+
+        self->cantina->perform(self->ports.input, self->outputBuffers, n_samples);
+
+    } catch (cant::CantinaException const &e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    float coef = db_gain_to_coef(*self->ports.gain);
+    merge_output(self, coef, n_samples);
+}
+
+LV2_SYMBOL_EXPORT
+LV2_Descriptor const *
+lv2_descriptor(uint32_t index) {
+    static LV2_Descriptor const descriptor = {
+        CANTINA_URI,
+        instantiate,
+        connect_port,
+        activate,
+        run,
+        deactivate,
+        cleanup,
+        extension_data
+    };
+    return index == 0 ? &descriptor : nullptr;
+}
